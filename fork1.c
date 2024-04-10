58abcf2308d761b766662354066bc9e538b9e984
Youngpil Kim <90808891+INUOSLab@users.noreply.github.com>
2024-04-10 14:12:43 +0900

    Create fork1.c

---
+++ fork1.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+
+int main(int argc, char* argv[]) {
+
+    printf("Process %d start\n",  (int)getpid());
+    int rc = fork();
+    if (rc < 0) {
+        // fork failed
+        fprintf(stderr, "fork failed\n");
+        exit(1);
+    } else if (rc == 0) {
+        // child
+        printf("I am child: %d\n", (int) getpid());        
+    } else {
+        printf("I am parent of %d, and I am %d\n", rc, (int) getpid());
+    }
+    return 0;
+}
